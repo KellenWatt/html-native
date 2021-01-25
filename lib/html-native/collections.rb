456f548c89c38cff0d29bd11190513c275a443bc
Kellen Watt <kbw6d9@mst.edu>
2021-01-24 22:36:55 -0600

    Refactor list methods to independent classes
    Having the concepts as methods was non-functional, since the element
    methods aren't available as a DSL within HTMLComponent itself. Classes
    are easier to implement and more accurately reflect proper use.

--- lib/html-native/collections.rb
+++ lib/html-native/collections.rb
@@ -1,29 +1,76 @@
+require "html-native"
+class OrderedListComponent
+  include HTMLComponent
+
+  def initialize(data)
+    @list_data = data
+  end
+
+  def render(attributes: {}, item_attributes: {}, &block)
+    ol(attributes) do
+      @list_data.inject(Builder.new) do |acc, l|
+        acc + li(item_attributes) do
+          block_given? ? block.call(l) : l.to_s
+        end
+      end
+    end
+  end
+end
+
+class UnorderedListComponent
+  include HTMLComponent
+
+  def initialize(data)
+    @list_data = data
+  end
+
+  def render(attributes: {}, item_attributes: {}, &block)
+    ul(attributes) do
+      @list_data.inject(Builder.new) do |acc, l|
+        acc + li(item_attributes) do
+          block_given? ? block.call(l) : l.to_s
+        end
+      end
+    end
+  end
+end
+
+class ListComponent
+  def initialize(data, ordered: false)
+    @list = ordered ? OrderedListComponent.new(data) : UnorderedListComponent.new(data)
+  end
+
+  def render(attributes: {}, item_attributes: {}, &block)
+    @list.render(attributes, item_attributes, &block)
+  end
+end
+
 module HTMLComponent
-  # lists
-  def ordered_list(list, attributes: {}, item_attributes: {}, &block)
-    ol(attributes) {
-      list.inject(Builder.new) do |acc, l|
-        acc + li(item_attributes) {
-                block_given? ? block.call(l) : l.to_s
-              }
-      end
-    }
-  end
-
-  def ordered_list(list, attributes: {}, item_attributes: {}, &block)
-    ul(attributes) {
-      list.inject(Builder.new) do |acc, l|
-        acc + li(item_attributes) {
-                block_given? ? block.call(l) : l.to_s
-              }
-      end
-    }
-  end
-
-  def list(list, attributes: {}, item_attributes: {}, ordered: false, &block)
-    self.__send__(ordered ? :ordered_list : :unordered_list, attributes, item_attributes, &block)
-  end
-
   # tables
 
+  # This could be more complete, but needs polishing and to consider the cases
+  # Doesn't work for now. Here for reference
+  def self.table_from(data, col_names: [], attributes: {}, header_attrs: {}, 
+                 row_attrs: {}, cell_attrs: {}, &block)
+    table(attributes) do
+      unless col_names.empty?
+        tr(row_attrs) do
+          col_names.inject(Builder.new) do |acc, c|
+            acc + th(cell_attrs.merge(header_attrs)) {c.to_s}
+          end
+        end 
+      end +
+      data.inject(Builder.new) do |acc, row|
+        acc + tr(row_attrs) do
+          if block_given? 
+            yield row
+          else
+            row.inject(Builder.new) do |acc, cell|
+              acc + td(cell_attrs) {cell.to_s} 
+            end
+          end
+        end
+      end
+    end
+  end
 end
