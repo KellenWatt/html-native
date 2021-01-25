3d13c17eba3c5d8d775228cd9fef1f1dfbcfd221
Kellen Watt <kbw6d9@mst.edu>
2021-01-25 01:26:43 -0600

    Create TableComponent class

--- lib/html-native/collections.rb
+++ lib/html-native/collections.rb
@@ -45,29 +45,81 @@ class ListComponent
   end
 end
 
-module HTMLComponent
-  # tables
+# This needs some reworking, since it's not intuitive
+class TableComponent
+  include HTMLComponent
+  # 2 types:
+  #   - 2d-Array table
+  #     - column names provided
+  #     - outer array contains rows, inner contain cells in their row
+  #     - specify if first row contains header or if separate.
+  #   - Hash table (from a hash)
+  #     - column names derived from keys
+  #     - keys associated with columns, each entry in value array is a 
+  #         row under the respective column
+  #     - header derived from keys
+ 
+  def initialize(header, rows)
+    @header = header
+    @rows = rows
+  end
 
-  # This could be more complete, but needs polishing and to consider the cases
-  # Doesn't work for now. Here for reference
-  def self.table_from(data, col_names: [], attributes: {}, header_attrs: {}, 
-                 row_attrs: {}, cell_attrs: {}, &block)
-    table(attributes) do
-      unless col_names.empty?
-        tr(row_attrs) do
-          col_names.inject(Builder.new) do |acc, c|
-            acc + th(cell_attrs.merge(header_attrs)) {c.to_s}
+  # header options:
+  # array - use as header
+  # symbol - if :from_data, then use first row, if :none, set @header to nil
+  def self.from_array(data, header: :none)
+    head = rows = nil
+    if header == :from_data
+      head = data[0]
+      rows = data[1..]
+    else
+      head = header.kind_of?(Array) ? header : nil
+      rows = data
+    end
+    new(head, rows)
+  end
+
+  def self.from_hash(data, vertical: true)
+    if vertical
+      header = data.keys
+      rowcount = data.values.map(&:length).max
+      rows = [] * rowcount
+      data.each do |k,col|
+        rowcount.times do |i|
+          rows[i] << (i < col.size ? col[i] : nil)
+        end
+      end
+      new(header, rows)
+    else
+      header = nil
+      rows = data.map do |k, v|
+        [k] + v
+      end
+      new(header, rows)
+    end
+  end
+
+  def render(attributes: {table: {}, header: {}, header_cell: {}, row: {}, cell: {}}, &block)
+    attributes[:table] ||= {}
+    attributes[:header] ||= {}
+    attributes[:header_cell] ||= {}
+    attributes[:row] ||= {}
+    attributes[:cell] ||= {}
+
+    table(attributes[:table]) do
+      if @header
+        tr(attributes[:row].merge(attributes[:header])) do
+          @header.inject(Builder.new) do |acc, h|
+            acc + th(attributes[:cell].merge(attributes[:header_cell])) {h}
           end
-        end 
+        end
+      else
+        Builder.new
       end +
-      data.inject(Builder.new) do |acc, row|
-        acc + tr(row_attrs) do
-          if block_given? 
-            yield row
-          else
-            row.inject(Builder.new) do |acc, cell|
-              acc + td(cell_attrs) {cell.to_s} 
-            end
+      @rows.inject(Builder.new) do |acc, row|
+        acc + tr(attributes[:row]) do
+          row.inject(Builder.new) do |mem, cell|
+            mem + td(attributes[:cell]) {block_given? ? yield(cell) : cell}
           end
         end
       end
