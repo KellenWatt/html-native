555dfe3e22cb179e92142eb65f2161990f9e00b5
Kellen Watt <kbw6d9@mst.edu>
2021-01-25 20:40:37 -0600

    Various changes
    - Simplify attribute parameters
    - Add component_map to Enumerable, for iterating over collections to
    produce sets component
    - Add Component classes for radio buttons and dropdown menus

--- lib/html-native/collections.rb
+++ lib/html-native/collections.rb
@@ -1,9 +1,32 @@
 require "html-native"
 require "html-native/builder"
+
+module Enumerable
+  def component_map 
+    if block_given?
+      result = Builder.new
+      each do |e|
+        result += yield(e)
+      end
+      result
+    else
+      to_enum(:component_map)
+    end
+  end
+
+  def to_ol(attributes: {})
+    OrderedListComponent.new(self, attributes: attributes)
+  end
+  
+  def to_ul(attributes: {})
+    UnorderedListComponent.new(self, attributes: attributes)
+  end
+end
+
 class OrderedListComponent
   include HTMLComponent
 
-  def initialize(data, attributes: {list: {}, item: {}})
+  def initialize(data, attributes: {})
     @list_data = data
     @list_attributes = attributes[:list] || {}
     @item_attributes = attributes[:item] || {}
@@ -11,9 +34,9 @@ class OrderedListComponent
 
   def render(&block)
     ol(@list_attributes) do
-      @list_data.inject(Builder.new) do |acc, l|
-        acc + li(@item_attributes) do
-          block_given? ? block.call(l) : l.to_s
+      @list_data.component_map do |l|
+        li(@item_attributes) do
+          block_given? ? yield(l) : l.to_s
         end
       end
     end
@@ -23,7 +46,7 @@ end
 class UnorderedListComponent
   include HTMLComponent
 
-  def initialize(data, attributes: {list: {}, item: {}})
+  def initialize(data, attributes: {})
     @list_data = data
     @list_attributes = attributes[:list] || {}
     @item_attributes = attributes[:item] || {}
@@ -31,9 +54,9 @@ class UnorderedListComponent
 
   def render(&block)
     ul(@list_attributes) do
-      @list_data.inject(Builder.new) do |acc, l|
-        acc + li(@item_attributes) do
-          block_given? ? block.call(l) : l.to_s
+      @list_data.component_map do |l|
+        li(@item_attributes) do
+          block_given? ? yield(l) : l.to_s
         end
       end
     end
@@ -41,7 +64,7 @@ class UnorderedListComponent
 end
 
 class ListComponent
-  def initialize(data, attributes: {list: {}, item: {}}, ordered: false)
+  def initialize(data, attributes: {}, ordered: false)
     @list = ordered ? OrderedListComponent.new(data, attributes) : 
                       UnorderedListComponent.new(data, attributes)
   end
@@ -53,8 +76,8 @@ end
 
 class TableRowComponent
   include HTMLComponent
-  
-  def initialize(data, attributes: {row: {}, cell: {}})
+
+  def initialize(data, attributes: {})
     @data = data
     @row_attributes = attributes[:row] || {}
     @cell_attributes = attributes[:cell] || {}
@@ -62,8 +85,8 @@ class TableRowComponent
 
   def render(&block)
     tr(@row_attributes) do
-      @data.inject(Builder.new) do |acc, c|
-        acc + td(@cell_attributes) {block_given? ? yield(c) : c}
+      @data.component_map |c|
+        td(@cell_attributes) {block_given? ? yield(c) : c}
       end
     end
   end
@@ -72,8 +95,8 @@ end
 # This needs some reworking, since it's not intuitive
 class TableComponent
   include HTMLComponent
- 
-  def initialize(header, rows, attributes: {table: {}, header: {}, header_cell: {}, row: {}, cell: {}})
+
+  def initialize(header, rows, attributes: {})
     @header = header
     @rows = rows
     @table_attributes = attributes[:table] || {}
@@ -86,7 +109,7 @@ class TableComponent
   # header options:
   # array - use as header
   # symbol - if :from_data, then use first row, if :none, set @header to nil
-  def self.from_array(data, attributes: {table: {}, header: {}, header_cell: {}, row: {}, cell: {}}, header: :none)
+  def self.from_array(data, attributes: {}, header: :none)
     head = rows = nil
     if header == :from_data
       head = data[0]
@@ -98,7 +121,7 @@ class TableComponent
     new(head, rows, attributes: attributes)
   end
 
-  def self.from_hash(data, attributes: {table: {}, header: {}, header_cell: {}, row: {}, cell: {}}, vertical: true)
+  def self.from_hash(data, attributes: {}, vertical: true)
     if vertical
       rowcount = data.values.map(&:length).max
       rows = [] * rowcount
@@ -120,16 +143,55 @@ class TableComponent
     table(@table_attributes) do
       if @header
         tr(@row_attributes.merge(@header_attributes)) do
-          @header.inject(Builder.new) do |acc, h|
-            acc + th(@cell_attributes.merge(@header_cell_attributes)) {h}
+          @header.component_map do |h|
+            th(@cell_attributes.merge(@header_cell_attributes)) {h}
           end
         end
       else
         Builder.new
       end +
-      @rows.inject(Builder.new) do |acc, row|
-        acc + TableRowComponent.new(row, attributes: {row: @row_attributes, cell: @cell_attributes}).render(&block)
+      @rows.component_map do |row|
+        TableRowComponent.new(row, attributes: {row: @row_attributes, cell: @cell_attributes}).render(&block)
       end
+    end
+  end
+end
+
+class DropdownComponent
+  include HTMLComponent
+
+  def initialize(choices, name, attributes: {})
+    @choices = choices
+    @name = name
+    @menu_attributes = attributes[:menu]
+    @item_attributes = attributes[:item]
+  end
+
+  def render(&block)
+    select(@menu_attributes.merge({name: @name, id: "#{@name}-dropdown"})) do
+      @choices.component_map |c|
+        option(@item_attributes.merge({value: c})) {block_given? ? yield(c) : c}
+      end
+    end
+  end
+end
+
+class RadioGroupComponent
+  include HTMLComponent
+
+  def initialize(choices, name, attributes: {}, labelled: true)
+    @choices = choices
+    @name = name
+    @button_attributes = attributes[:button]
+    @label_attributes = attributes[:label]
+    @labelled = labelled
+  end
+
+  def render(&block)
+    @choices.component_map do |c|
+      id = "#{@name}-#{c}" 
+      input({type: "radio", id: id, name: @name, value: c}) +
+        (@labelled ? (label({for: id}) {block_given? ? yield(c) : c}) : nil)
     end
   end
 end
